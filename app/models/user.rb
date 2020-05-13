b91aa7a942c95370fad20af40bbc939dcf1a6801
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-05-13 13:58:26 +0530

    add model User and validiations

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  validates :username, presence: true, 
+                    length: { minimun: 2 , maximum: 25 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true,
+                  uniqueness: { case_sensitive: false}, 
+                  length: { maximum: 50 },
+                    format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimun: 4, maximum: 15 }
+end
